14da212eb9bb4169a5612abcfe8324d18239c868
0hyunU <ryh2803@gmail.com>
2022-07-10 02:09:54 +0900

    AppendixA

---
+++ StudyLog/DLFS3/steps/AppendixA.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+
+                #기존
+                # x.grad = gx
+                # 하지만 같은 변수를 사용했을 때
+                # 전파받은 grad는 더해주어야 한다.
+
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad += gx
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+def add(x0,x1):
+    return Add()(x0,x1)
+x = Variable(np.array(3.0))
+y = add(x, x)
+y.backward()
+
+
+# x.grad += gx를 하지 않고
+# x.grad = x.grad + gx를 하는 이유
+
+print('y.grad:{}({}()'.format(y.grad,id(y.grad)))
+print('x.grad:{}({}()'.format(x.grad,id(x.grad)))
